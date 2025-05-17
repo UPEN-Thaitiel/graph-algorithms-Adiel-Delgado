b44083b860ed34940ae741f62cec86a8a80a174a
Adiel_Delgado <nefthali1107@outlook.com>
2025-05-17 13:38:36 -0700

    I complete the activity using BFS and queue, it was hard for me to understand how the BFS works.

--- MazeProblem.py
+++ MazeProblem.py
@@ -37,7 +37,44 @@ Good luck!
 
 
 """
+import queue
 
+def solve(maze):
+    #This would let us know if the end or the start are blocked, if this happends the maze can´t be solved.
+    n, m = len(maze), len(maze[0])
+    if maze [0][0] == 0 or maze[n-1][m-1] == 0:
+        return None
+    #This would let us know or track the paths that are visited to no loop
+    visited = [[False]*m for _ in range(n)]
+    visited[0][0] = True
+    #This would create or FIFO queue to do our BFS
+    q = queue.Queue()
+    q.put((0, 0, [(0, 0)]))
+    #We define the directions of our path finder, down, up, right, left in that order.
+    directions =[(1,0), (-1,0), (0,1), (0,-1)]
+    
+    while not q.empty():
+        x, y, path =q.get()
+    #This would mark the short path found
+        if (x, y) == (n-1, m-1):
+            solved = [row[:] for row in maze]
+            for px, py in path:
+                solved[px][py] = "S"
+            return solved
+    #This would explore the neighbors of the paths
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if (0 <= nx < n and 0 <= ny < m and maze[nx][ny] == 1 and not visited[nx][ny]):
+                visited[nx][ny] = True
+    #This would check the limit of every maze
+                q.put((nx, ny, path + [(nx, ny)]))
+    return None
+    #This would help us to print the mazes row per
+def print_maze(maze):
+    for row in maze:
+        print(' '.join(str(cell) for cell in row))
+    print()
+    
 if __name__ == '__main__':
     ### Your code must succesfully solve the following mazes:
     
@@ -72,4 +109,13 @@ if __name__ == '__main__':
         [1, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1]
     ]
-
+    for name, maze in [('m', m),
+                       ('easy_maze', easy_maze),
+                       ('medium_maze', medium_maze),
+                       ('hard_maze', hard_maze)]:
+        print(f"--- {name} ---")
+        sol = solve(maze)
+        if sol is None:
+            print("There is not a path.\n")
+        else:
+            print_maze(sol)
